ddc3149a0249a1525639d58158a6567876cceff8
Kevin Mitchell <>
2024-02-16 16:32:58 +0900

    add config to make it possible to enable or enable functionality
    * add controller actions `/share/cart` and `/share/token` that can be used instead of section data
    * allow the auto creation of tokens or the auto creation of guest carts via configuration

--- Controller/Cart/Index.php
+++ Controller/Cart/Index.php
@@ -1,34 +1,27 @@
 <?php
-
 declare(strict_types=1);
 
-namespace Atama\Share\CustomerData;
+namespace Atama\Share\Controller\Cart;
 
-use Magento\Customer\CustomerData\SectionSourceInterface;
+use Magento\Checkout\Model\Session;
 use Magento\Customer\Helper\Session\CurrentCustomer;
+use Magento\Framework\App\Action\HttpPostActionInterface;
 use Magento\Framework\App\Config\ScopeConfigInterface;
-use Magento\Framework\App\ObjectManager;
-use Magento\Integration\Api\UserTokenIssuerInterface;
-use Magento\Integration\Model\CustomUserContext;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\GraphQl\Model\Query\ContextInterface;
 use Magento\Quote\Model\GuestCart\GuestCartResolver;
 use Magento\Quote\Model\QuoteIdToMaskedQuoteIdInterface;
-use Magento\Checkout\Model\Session;
-use Magento\Integration\Model\UserToken\UserTokenParametersFactory;
 use Magento\QuoteGraphQl\Model\Cart\CreateEmptyCartForCustomer;
-use Magento\GraphQl\Model\Query\ContextInterface;
 
 /**
- * Responsible for providing data for a new "side-by-side" customer data section.
- *
- * This class handles both guest and registered customers and will provide the cart id
- * and (in the case of registered customers) a bearer token that can be used to authenticate
- * graphql requests coming from sources outside of Magento.
+ * Route handler for returning the current session cart for either guest or customer, and creating
+ * one if needed. This route needs to be enabled via config.
  */
-class SideBySide implements SectionSourceInterface
+class Index implements HttpPostActionInterface
 {
-    private const SIDE_BY_SIDE_TOKEN_ENABLED_PATH = 'web/edge_delivery_service/side_by_side_section_token_enable';
-    private const SIDE_BY_SIDE_CART_CREATION_ENABLED_PATH = 'web/edge_delivery_service/side_by_side_section_create_cart_enable';
 
+    private const SHARE_SESSION_CREATE_CART_ROUTE_ENABLED_PATH = 'web/edge_delivery_service/side_by_side_create_cart_route_enable';
     /**
      * @var QuoteIdToMaskedQuoteIdInterface
      */
@@ -37,6 +30,10 @@ class SideBySide implements SectionSourceInterface
      * @var \Magento\Checkout\Model\Session
      */
     private $session;
+    /**
+     * @var ResultFactory
+     */
+    protected $resultFactory;
     /**
      * @var ScopeConfigInterface
      */
@@ -53,53 +50,48 @@ class SideBySide implements SectionSourceInterface
      * @var CreateEmptyCartForCustomer
      */
     private $createEmptyCartForCustomer;
-    /**
-     * @var UserTokenParametersFactory
-     */
-    private $tokenParametersFactory;
-    /**
-     * @var UserTokenIssuerInterface
-     */
-    private $tokenIssuer;
-
 
 
     /**
      * @param QuoteIdToMaskedQuoteIdInterface $quoteIdToMaskedQuoteId
      * @param Session $session
+     * @param ResultFactory $resultFactory
      * @param ScopeConfigInterface $scopeConfig
      * @param CurrentCustomer $currentCustomer
      * @param GuestCartResolver $guestCartResolver
      * @param CreateEmptyCartForCustomer $createEmptyCartForCustomer
-     * @param UserTokenParametersFactory|null $tokenParamsFactory
-     * @param UserTokenIssuerInterface|null $tokenIssuer
      */
     public function __construct(
         QuoteIdToMaskedQuoteIdInterface $quoteIdToMaskedQuoteId,
         Session $session,
+        ResultFactory $resultFactory,
         ScopeConfigInterface $scopeConfig,
         CurrentCustomer $currentCustomer,
         GuestCartResolver $guestCartResolver,
         CreateEmptyCartForCustomer $createEmptyCartForCustomer,
-        ?UserTokenParametersFactory $tokenParamsFactory = null,
-        ?UserTokenIssuerInterface $tokenIssuer = null,
     ) {
         $this->quoteIdToMaskedQuoteId = $quoteIdToMaskedQuoteId;
         $this->session = $session;
+        $this->resultFactory = $resultFactory;
         $this->scopeConfig = $scopeConfig;
         $this->currentCustomer = $currentCustomer;
         $this->guestCartResolver = $guestCartResolver;
         $this->createEmptyCartForCustomer = $createEmptyCartForCustomer;
-        $this->tokenParametersFactory = $tokenParamsFactory
-            ?? ObjectManager::getInstance()->get(UserTokenParametersFactory::class);
-        $this->tokenIssuer = $tokenIssuer ?? ObjectManager::getInstance()->get(UserTokenIssuerInterface::class);
     }
-
     /**
-     * @inheritdoc
+     * Execute view action
+     *
+     * @return ResultInterface
      */
-    public function getSectionData(): array
+    public function execute()
     {
+        // If not enabled in config return a not found type response
+        if (!$this->isSessionCreateCartRouteEnabled()) {
+            $resultForward = $this->resultFactory->create(ResultFactory::TYPE_FORWARD);
+            $resultForward->forward('noroute');
+            return $resultForward;
+        }
+
         $cart = null;
         /**
          * @var ContextInterface $context
@@ -111,53 +103,34 @@ class SideBySide implements SectionSourceInterface
             $maskedCartId = null;
             if (is_numeric($guestQuoteId) && is_int(intval($guestQuoteId))) {
                 $maskedCartId = $this->quoteIdToMaskedQuoteId->execute(intval($guestQuoteId));
-            } else if($this->isCartCreationEnabled()) {
+            } else {
                 $guestQuote = $this->guestCartResolver->resolve();
                 $guestQuoteId = is_numeric($guestQuote->getId()) && is_int(intval($guestQuote->getId())) ? intval($guestQuote->getId()) : null;
                 $this->session->setQuoteId($guestQuote->getId());
                 $this->session->setCartWasUpdated(true);
                 $maskedCartId = $this->quoteIdToMaskedQuoteId->execute($guestQuoteId);
             }
-
-            return [
-                "cart_id" => $maskedCartId,
-            ];
         } else {
             $currentCustomerId = $this->currentCustomer->getCustomer()->getId();
 
             $maskedCartId = null;
             if ($this->session->getQuoteId() !== null && $this->session->getQuoteId() !== 0) {
                 $maskedCartId = $this->quoteIdToMaskedQuoteId->execute(intval($this->session->getQuoteId()));
-            } else if ($this->isCartCreationEnabled()) {
+            } else {
                 $maskedCartId = $this->createEmptyCartForCustomer->execute($currentCustomerId);
             }
-
-
-            return [
-                "cart_id" => $maskedCartId,
-                "token" => $this->isTokenCreationEnabled() ? $this->generateCustomerToken(intval($currentCustomerId)) : null,
-            ];
         }
 
+        $rawResult = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+        $rawResult->setData([
+            'cart_id' => $maskedCartId
+        ]);
+       return $rawResult;
     }
 
-    private function generateCustomerToken(int $customerId): string {
-        $context = new CustomUserContext(
-            $customerId,
-            CustomUserContext::USER_TYPE_CUSTOMER
-        );
-        $params = $this->tokenParametersFactory->create();
-
-        return $this->tokenIssuer->create($context, $params);
-    }
-
-    private function isTokenCreationEnabled()
+    private function isSessionCreateCartRouteEnabled()
     {
-        return $this->scopeConfig->getValue(self::SIDE_BY_SIDE_TOKEN_ENABLED_PATH) === "1";
+        return $this->scopeConfig->getValue(self::SHARE_SESSION_CREATE_CART_ROUTE_ENABLED_PATH) === "1";
     }
 
-    private function isCartCreationEnabled()
-    {
-        return $this->scopeConfig->getValue(self::SIDE_BY_SIDE_CART_CREATION_ENABLED_PATH) === "1";
-    }
 }
